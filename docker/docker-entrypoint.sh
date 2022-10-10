07eff0b214e944ab5bf42aa5059a71b3b8b0c775
horiokart <chuson.dev@gmail.com>
2022-10-10 23:38:29 +0900

    update docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #echo ROS_IP=192.168.0.10 >> $HOME/.bashrc
 #echo ROS_MASTER_URI=http://$ROS_IP:11311 >> $HOME/.bashrc
-echo ROS_IP=$ROS_IP >> $HOME/.bashrc
-echo ROS_MASTER_URI=http://$ROS_IP:11311 >> $HOME/.bashrc
-echo "source /opt/ros/noetic/setup.bash" >> $HOME/.bashrc
-echo "source $HOME/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+#echo ROS_IP=$ROS_IP >> $HOME/.bashrc
+#echo ROS_MASTER_URI=http://$ROS_IP:11311 >> $HOME/.bashrc
+#echo "source /opt/ros/noetic/setup.bash" >> $HOME/.bashrc
+#echo "source $HOME/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+#
+#source /opt/ros/noetic/setup.bash
+#cd $HOME/catkin_ws
+#rosdep install -i -y --from-paths src/
+#catkin build
+#source $HOME/catkin_ws/devel/setup.bash
 
-source /opt/ros/noetic/setup.bash
-cd $HOME/catkin_ws
-rosdep install -i -y --from-paths src/
-catkin build
-source $HOME/catkin_ws/devel/setup.bash
+#roscore
+sleep infinity
 
-roscore
