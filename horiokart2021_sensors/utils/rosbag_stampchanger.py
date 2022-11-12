c7300e688784380e3acb5a641fe65330389dfb86
chuson <chuson.dev@gmail.com>
2022-11-13 01:34:45 +0900

    update rosbag modify tool

--- horiokart2021_sensors/utils/rosbag_stampchanger.py
+++ horiokart2021_sensors/utils/rosbag_stampchanger.py
@@ -1,8 +1,9 @@
 import rosbag
 import os
+import pprint
 
 
-bag_file_name = r"/home/chuson/ros_data/20220917_rosbag/2022-09-17-03-07-58-001.bag"
+bag_file_name = r"/root/ros_data/20221106/slam_2022-11-06-04-59-06.bag"
 f_name, suf = os.path.splitext(os.path.basename(bag_file_name))
 output_file_name = os.path.join(
     os.path.dirname(bag_file_name),
@@ -12,6 +13,9 @@ output_file_name = os.path.join(
 # bagのclockをstampにする
 with rosbag.Bag(output_file_name, "w") as outbag:
     for topic, msg, t in rosbag.Bag(bag_file_name).read_messages():
+        print("---")
+        print(topic)
+        pprint.pprint(msg)
         if topic == "/tf" and msg.transforms:
             outbag.write(topic, msg, msg.transforms[0].header.stamp)
         else:
@@ -21,6 +25,6 @@ with rosbag.Bag(output_file_name, "w") as outbag:
                 continue
             
             # frame_id先頭"/"除去
-            if hasattr(msg.header, "frame_id") and msg.header.frame_id[0] == "/":
+            if hasattr(msg.header, "frame_id") and msg.header.frame_id !="" and msg.header.frame_id[0] == "/":
                 msg.header.frame_id = msg.header.frame_id[1:]
             outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
