4dee8377d62824830ac5719831c3dda4d2b88c04
Chu-son <chuson.dev@gmail.com>
2023-06-18 08:12:47 +0000

    rename description launch

--- horiokart_description/launch/bringup.launch.py
+++ horiokart_description/launch/bringup.launch.py
@@ -15,20 +15,8 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     package_name = "horiokart_description"
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    simulation_arg = LaunchConfiguration('simulation')
     xacro_file_name = LaunchConfiguration('xacro_file_name')
-
-
-    # xacro_file_path = os.path.join(
-    #     get_package_share_directory(package_name),
-    #     'urdf',
-    #     xacro_file_name)
-
-    xacro_file_path = PathJoinSubstitution([
-        get_package_share_directory(package_name),
-        'urdf',
-        xacro_file_name,
-    ])
 
     robot_description_content = Command(
         [
@@ -40,17 +28,12 @@ def generate_launch_description():
         ]
     )
 
-
     robot_state_publisher_node = Node(
-    package='robot_state_publisher',
-    executable='robot_state_publisher',
-    parameters=[
-        #{
-        # 'robot_description': ParameterValue(
-        #     Command(['xacro ', xacro_file_path]), value_type=str
-        # )},
-        {'robot_description': robot_description_content},
-        # {'use_sim_time': use_sim_time},
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_description_content},
+            {'use_sim_time': simulation_arg},
         ]
     )
 
@@ -58,26 +41,22 @@ def generate_launch_description():
         package="joint_state_publisher",
         executable="joint_state_publisher",
         output="screen",
-    )
-
-    joint_state_pub_gui_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-        output="screen",
+        parameters=[{
+            "use_sim_time": simulation_arg,
+        }],
     )
 
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
+            'simulation',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'
-            ),
+            description='Use simulation clock if true'
+        ),
         DeclareLaunchArgument(
             'xacro_file_name',
             default_value="horiokart_description.urdf.xacro",
             description='Xacro file to use for URDF'
-            ),
+        ),
         robot_state_publisher_node,
         joint_state_pub_node,
-        # joint_state_pub_gui_node,
     ])
