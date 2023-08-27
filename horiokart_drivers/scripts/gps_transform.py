8a38142eff583e8743be8da94ec380a1c0320ce1
Chu-son <chuson.dev@gmail.com>
2023-08-27 07:09:58 +0000

    update gps transform

--- horiokart_drivers/scripts/gps_transform.py
+++ horiokart_drivers/scripts/gps_transform.py
@@ -3,6 +3,9 @@
 from pyproj import Proj
 import math
 import dataclasses
+import numpy as np
+from scipy.optimize import minimize
+import yaml
 
 
 @dataclasses.dataclass
@@ -12,7 +15,16 @@ class Pose:
     yaw: float
 
 
+@dataclasses.dataclass
+class UTMtoMAP_RefPoint:
+    utm: Pose  # utm_position. unit: m. don't use yaw
+    map: Pose  # map_position. unit: m. don't use yaw
+
+
 class GpsTransform:
+    UTM2MAP_APPEND_THRESHOLD = 30  # unit: m
+    NUM_USE_UTM2MAP_REFPOINT = 3
+
     def __init__(self, offset_yaw: float = 0.0):
         self._default_offset_yaw = offset_yaw
 
@@ -22,6 +34,134 @@ class GpsTransform:
         self.proj = None
         self.origin_utm = None
         self._previous_pose = None
+        self._last_utm = None
+
+        self._utm_to_map_list = []
+
+    @property
+    def last_utm(self):
+        return self._last_utm if self._last_utm is not None else Pose(0, 0, 0)
+
+    def add_utm_to_map_refpoint(self, utm: Pose, map: Pose):
+        if len(self._utm_to_map_list) == 0:
+            self._utm_to_map_list.append(UTMtoMAP_RefPoint(utm, map))
+        elif (utm.x - self._utm_to_map_list[-1].utm.x)**2 + (utm.y - self._utm_to_map_list[-1].utm.y)**2 > self.UTM2MAP_APPEND_THRESHOLD ** 2:
+            self._utm_to_map_list.append(UTMtoMAP_RefPoint(utm, map))
+
+    def save_utm_to_map_list(self, file_path: str):
+        with open(file_path, 'w') as f:
+            yaml.dump(self._utm_to_map_list, f)
+
+    def load_utm_to_map_list(self, file_path: str):
+        with open(file_path, 'r') as f:
+            self._utm_to_map_list = yaml.load(f, Loader=yaml.FullLoader)
+
+        print(self._utm_to_map_list)
+
+    def find_point_d(self, a, b, c, ad_distance, bd_distance, cd_distance):
+        # 点A、B、Cの座標を取得
+        xa, ya = a
+        xb, yb = b
+        xc, yc = c
+
+        # 点Aから点Dまでのベクトルを計算
+        ad_vector = [xa - xb, ya - yb]
+        # 点Bから点Dまでのベクトルを計算
+        bd_vector = [xb - xa, yb - ya]
+        # 点Cから点Dまでのベクトルを計算
+        cd_vector = [xc - xa, yc - ya]
+
+        # ベクトルの大きさを求める
+        ad_magnitude = math.sqrt(ad_vector[0]**2 + ad_vector[1]**2)
+        bd_magnitude = math.sqrt(bd_vector[0]**2 + bd_vector[1]**2)
+        cd_magnitude = math.sqrt(cd_vector[0]**2 + cd_vector[1]**2)
+
+        # 各ベクトルを距離に合わせてスケーリング
+        ad_scaled = [ad_vector[0] * ad_distance / ad_magnitude,
+                     ad_vector[1] * ad_distance / ad_magnitude]
+        bd_scaled = [bd_vector[0] * bd_distance / bd_magnitude,
+                     bd_vector[1] * bd_distance / bd_magnitude]
+        cd_scaled = [cd_vector[0] * cd_distance / cd_magnitude,
+                     cd_vector[1] * cd_distance / cd_magnitude]
+
+        # 点Dの座標を計算
+        xd = xa + ad_scaled[0] + bd_scaled[0] + cd_scaled[0]
+        yd = ya + ad_scaled[1] + bd_scaled[1] + cd_scaled[1]
+
+        return xd, yd
+
+    def distance_error(self, point, known_points, distances):
+        errors = []
+        for i in range(len(known_points)):
+            error = np.linalg.norm(known_points[i] - point) - distances[i]
+            errors.append(error)
+        return errors
+
+    def estimate_point(self, known_points, distances, initial_guess):
+        result = minimize(
+            lambda p: np.sum(
+                np.square(self.distance_error(p, known_points, distances))),
+            initial_guess
+        )
+        estimated_point = result.x
+        return estimated_point
+
+    def calc_utm_to_map_offset(self, utm: Pose) -> Pose:
+        if len(self._utm_to_map_list) == 0:
+            return Pose(
+                utm.x,
+                utm.y,
+                self._default_offset_yaw)
+
+        # use first data. need yaw
+        elif len(self._utm_to_map_list) == 1:
+            # calculate offset utm to map from first data
+            offset_x = self._utm_to_map_list[0].map.x - \
+                self._utm_to_map_list[0].utm.x
+            offset_y = self._utm_to_map_list[0].map.y - \
+                self._utm_to_map_list[0].utm.y
+            offset_yaw = self._utm_to_map_list[0].utm.yaw
+
+            return Pose(offset_x, offset_y, offset_yaw)
+
+        # use multi data. don't use yaw
+        elif len(self._utm_to_map_list) >= 2:
+            distance_list = []
+            for utm_to_map in self._utm_to_map_list:
+                distance_list.append(
+                    math.sqrt((utm.x - utm_to_map.utm.x)**2 + (utm.y - utm_to_map.utm.y)**2)
+                )
+
+            # get sorted index by distance
+            sorted_index = sorted(
+                range(len(distance_list)),
+                key=lambda k: distance_list[k])
+            # get top N utm_to_map
+            use_utm_to_map_list = []
+            use_distance_list = []
+            for i in range(min(self.NUM_USE_UTM2MAP_REFPOINT, len(sorted_index))):
+                use_utm_to_map_list.append(
+                    self._utm_to_map_list[sorted_index[i]])
+                use_distance_list.append(distance_list[sorted_index[i]])
+
+            # # calculate offset utm to map each data
+            # offset_x_list = []
+            # offset_y_list = []
+            # for utm_to_map in use_utm_to_map_list:
+            #     offset_x_list.append(
+            #         utm_to_map.map.x - utm_to_map.utm.x)
+            #     offset_y_list.append(
+            #         utm_to_map.map.y - utm_to_map.utm.y)
+            # # calculate average offset utm to map
+            # offset_x = sum(offset_x_list) / len(offset_x_list)
+            # offset_y = sum(offset_y_list) / len(offset_y_list)
+            offset_x, offset_y = self.estimate_point(
+                known_points=np.array([[utm_to_map.map.x, utm_to_map.map.y] for utm_to_map in use_utm_to_map_list]),
+                distances=np.array(use_distance_list),
+                initial_guess=np.array([0.0, 0.0])
+            )
+
+            return Pose(offset_x, offset_y, 0.0)
 
     def to_utm(self, lat, lon) -> Pose:
         if self.proj is None:
@@ -32,18 +172,38 @@ class GpsTransform:
         return Pose(x, y, 0.0)
 
     def transform_to_map(self, lat, lon) -> Pose:
-        utm_position = self.to_utm(lat, lon)
+        self._last_utm = self.to_utm(lat, lon)
+        if self.origin_utm is None:
+            self.origin_utm = Pose(
+                self._last_utm.x,
+                self._last_utm.y,
+                self._default_offset_yaw)
+
+        utm_to_map_offset = self.calc_utm_to_map_offset(self._last_utm)
+        print("---------------")
+        print(utm_to_map_offset)
+        print("---------------")
+
+        return Pose(
+            # self._last_utm.x + utm_to_map_offset.x,
+            # self._last_utm.y + utm_to_map_offset.y,
+            utm_to_map_offset.x,
+            utm_to_map_offset.y,
+            utm_to_map_offset.y)
+
+    def _transform_to_map(self, lat, lon) -> Pose:
+        self._last_utm = self.to_utm(lat, lon)
 
         # calculate offset from origin
         if self.origin_utm is None:
             self.origin_utm = Pose(
-                utm_position.x,
-                utm_position.y,
+                self._last_utm.x,
+                self._last_utm.y,
                 self._default_offset_yaw)
 
         map_position = Pose(
-            utm_position.x - self.origin_utm.x,
-            utm_position.y - self.origin_utm.y,
+            self._last_utm.x - self.origin_utm.x,
+            self._last_utm.y - self.origin_utm.y,
             self.origin_utm.yaw)
 
         # rotate
@@ -84,7 +244,7 @@ class GpsTransform:
         distance = math.sqrt(
             (robot_pose_of_map.x - self._previous_pose.x)**2 +
             (robot_pose_of_map.y - self._previous_pose.y)**2)
-        if distance < 0.3: # unit: m
+        if distance < 0.3:  # unit: m
             return
 
         current_yaw = math.atan2(
@@ -94,4 +254,3 @@ class GpsTransform:
         diff_yaw = actual_yaw - current_yaw
 
         self.origin_utm.yaw += diff_yaw
-
--- horiokart_drivers/scripts/gps_transform_node.py
+++ horiokart_drivers/scripts/gps_transform_node.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 import math
 
 import rclpy
 from rclpy.node import Node
 
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Pose
 from sensor_msgs.msg import NavSatFix
 from std_srvs.srv import Trigger
 
 import tf2_ros
+import tf2_py
 from tf_transformations import quaternion_from_euler, euler_from_quaternion
 
 from gps_transform import GpsTransform
@@ -21,8 +23,35 @@ class GpsTransformNode(Node):
     def __init__(self):
         super().__init__('gps_transform_node')
         self.get_logger().info('gps_transform_node started')
-        self.init_parameters()
 
+        self.init_parameters()
+        self.prepare_tf()
+        self.init_ros_communication()
+
+    def init_parameters(self):
+        self.resistration = self.declare_parameter(
+            'resistration',
+            True).value
+        self.map_frame_id = self.declare_parameter(
+            'map_frame_id',
+            'map').value
+        self.gps_frame_id = self.declare_parameter(
+            'gps_frame_id',
+            'gps_link').value
+        self.robot_base_frame_id = self.declare_parameter(
+            'robot_base_frame_id',
+            'base_link').value
+        self.utm_param_yaml_path = self.declare_parameter(
+            'utm_param_yaml_path',
+            '/root/ros2_data/utm_to_map_refpoint.yaml').value
+
+        self._gps_transform = GpsTransform(offset_yaw=2.75)
+
+        self.odom_msg = Odometry()
+        self.odom_msg.header.frame_id = self.map_frame_id
+        self.odom_msg.child_frame_id = self.gps_frame_id
+
+    def init_ros_communication(self):
         self.gps_sub = self.create_subscription(
             NavSatFix,
             'gps/fix',
@@ -34,10 +63,7 @@ class GpsTransformNode(Node):
             Odometry,
             'odom/gps',
             1)
-
-        self.odom_msg = Odometry()
-        self.odom_msg.header.frame_id = 'map'
-        self.odom_msg.child_frame_id = ''
+        self.odom_pub
 
         self.current_odom_msg = None
         self.odom_sub = self.create_subscription(
@@ -51,13 +77,28 @@ class GpsTransformNode(Node):
             'set_resistration_mode',
             self.set_resistration_mode_callback)
 
-    def init_parameters(self):
-        self.resistration = False
-        self._gps_transform = GpsTransform(offset_yaw=2.75)
+        self.save_utm_refpoints_srv = self.create_service(
+            Trigger,
+            'save_utm_refpoints',
+            self.save_utm_refpoint_callback)
 
+    def prepare_tf(self):
         # prepare tf2 listener
         self._tf_buffer = tf2_ros.Buffer()
         self._tf_listener = tf2_ros.TransformListener(self._tf_buffer, self)
+
+        try:
+            self._base_link_to_gps_transform = self._tf_buffer.lookup_transform(
+                self.robot_base_frame_id,  # target frame
+                self.gps_frame_id,  # source frame
+                rclpy.time.Time()
+            )
+        except Exception as e:
+            self.get_logger().info('tf not found')
+            return
+
+        self._gps_to_base_link_transform = tf2_py.inverse_transform(
+            self._base_link_to_gps_transform)
 
     def gps_callback(self, msg):
         map_position = self._gps_transform.transform_to_map(
@@ -67,8 +108,8 @@ class GpsTransformNode(Node):
             # listen tf from map to base_link
             try:
                 transform = self._tf_buffer.lookup_transform(
-                    "map",
-                    "base_link",
+                    self.map_frame_id,  # target frame
+                    self.gps_frame_id,  # source frame
                     rclpy.time.Time()
                 )
             except Exception as e:
@@ -83,40 +124,48 @@ class GpsTransformNode(Node):
             self.get_logger().info(
                 f"before offset yaw: {self._gps_transform.origin_utm.yaw}")
 
-            self._gps_transform.feedback_pose(
-                GpsPose(
-                    x=transform.transform.translation.x,
-                    y=transform.transform.translation.y,
-                    yaw=euler_from_quaternion([
-                        transform.transform.rotation.x,
-                        transform.transform.rotation.y,
-                        transform.transform.rotation.z,
-                        transform.transform.rotation.w
-                    ])[2]
-                )
+            map_frame_pose = GpsPose(
+                x=transform.transform.translation.x,
+                y=transform.transform.translation.y,
+                yaw=euler_from_quaternion([
+                    transform.transform.rotation.x,
+                    transform.transform.rotation.y,
+                    transform.transform.rotation.z,
+                    transform.transform.rotation.w
+                ])[2]
             )
+
+            self._gps_transform.feedback_pose(map_frame_pose)
+            self._gps_transform.add_utm_to_map_refpoint(
+                self._gps_transform.last_utm, map_frame_pose)
+
             self.get_logger().info('feedback pose to gps')
             self.get_logger().info(
                 f"after offset yaw: {self._gps_transform.origin_utm.yaw}")
 
-        self.odom_msg.header.stamp = msg.header.stamp
-        self.odom_msg.pose.pose.position.x = map_position.x
-        self.odom_msg.pose.pose.position.y = map_position.y
-        self.odom_msg.pose.pose.position.z = 0.0
-        # set orientation from yaw
-        q = quaternion_from_euler(0.0, 0.0, map_position.yaw)
-        self.odom_msg.pose.pose.orientation = Quaternion(
-            x=q[0], y=q[1], z=q[2], w=q[3])
+            self.get_logger().info(f"len: {len(self._gps_transform._utm_to_map_list)}")
+            self.get_logger().info("-----")
 
-        # set covariance
-        self.odom_msg.pose.covariance[0] = msg.position_covariance[0]
-        self.odom_msg.pose.covariance[7] = msg.position_covariance[4]
-        self.odom_msg.pose.covariance[14] = 0.0
-        self.odom_msg.pose.covariance[21] = 0.0
-        self.odom_msg.pose.covariance[28] = 0.0
-        self.odom_msg.pose.covariance[35] = 0.0
+        # else:
+        if True:
+            self.odom_msg.header.stamp = msg.header.stamp
+            self.odom_msg.pose.pose.position.x = map_position.x
+            self.odom_msg.pose.pose.position.y = map_position.y
+            self.odom_msg.pose.pose.position.z = 0.0
+            # set orientation from yaw
+            q = quaternion_from_euler(0.0, 0.0, map_position.yaw)
+            self.odom_msg.pose.pose.orientation = Quaternion(
+                x=q[0], y=q[1], z=q[2], w=q[3])
 
-        self.odom_pub.publish(self.odom_msg)
+            # set covariance
+            self.odom_msg.pose.covariance[0] = msg.position_covariance[0]
+            self.odom_msg.pose.covariance[7] = msg.position_covariance[4]
+            self.odom_msg.pose.covariance[14] = 0.0
+            self.odom_msg.pose.covariance[21] = 0.0
+            self.odom_msg.pose.covariance[28] = 0.0
+            self.odom_msg.pose.covariance[35] = 0.0
+
+            self.odom_pub.publish(self.odom_msg)
 
     def odom_callback(self, msg):
         self.current_odom_msg = msg
@@ -125,6 +174,22 @@ class GpsTransformNode(Node):
         self.resistration = request.data
         response.success = True
         response.message = f'set resistration mode to {self.resistration}'
+        return response
+
+    def save_utm_refpoint_callback(self, request, response):
+        if os.path.exists(self.utm_param_yaml_path):
+            dir_path = os.path.dirname(self.utm_param_yaml_path)
+            basename = os.path.basename(self.utm_param_yaml_path)
+            filename, ext = os.path.splitext(basename)
+
+            save_file_path = os.path.join(
+                dir_path, filename + '_new' + ext)
+        else:
+            save_file_path = self.utm_param_yaml_path
+
+        self._gps_transform.save_utm_refpoints(save_file_path)
+        response.success = True
+        response.message = f'saved utm refpoints to {self.utm_param_yaml_path}'
         return response
 
 
